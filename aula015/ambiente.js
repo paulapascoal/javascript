794a5bef083b4bada774b7a0f5c89fb1a5b695e3
paulapascoal <paulaiasmimpascoal2012@gmail.com>
2025-04-27 19:50:01 -0300

    curso de JS

---
+++ aula015/ambiente.js
@@ -0,0 +1,5 @@
+let num = [5, 8, 2, 9, 3]
+num.push(313)
+//console.log (`O vetor tem ${num.sort()} posições.`)
+let pos = num.indexOf(8)
+console.log (`O valor 8 está na posição ${pos}`)
---
+++ aula017/objeto01.js
@@ -0,0 +1,4 @@
+var amigo = {nome: 'Jose', sexo: 'M', peso: 85.4, engordar (p){
+
+}}
+console.log (`${amigo.nome} pesa ${amigo.peso}Kg.`)
